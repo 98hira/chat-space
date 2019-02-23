33095671db7ebed610f050ee001cb24349c19461
98hira <flattail2018@gmail.com>
2019-02-23 19:59:39 +0900

    [add]Userモデルにnickname設定を追加。
    以下コミットで追加したマイグレーションファイルをロールバックして、
    nicknameカラムの設定を追加した後に「rake db:migrate」した。
    49b3022667da850dea93ec7da5eba3badd23c678
    
    [time]2019/02/23-20:02

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,7 @@
 ActiveRecord::Schema.define(version: 2019_02_23_105257) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "nickname", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
@@ -21,6 +22,7 @@ ActiveRecord::Schema.define(version: 2019_02_23_105257) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["nickname"], name: "index_users_on_nickname"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
