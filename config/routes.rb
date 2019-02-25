f770184aa1bc0e74e2f0917d43f77d5073e79b74
98hira <flattail2018@gmail.com>
2019-02-25 15:39:32 +0900

    [add]ログイン/ログアウト時にflashメッセージを表示する。
    ログイン/ログアウト時のflashメッセージはdeviseがflash変数に格納しているので、
    それを表示する処理を追加するだけで対応できる。
    
    [time]2019/02/25-15:41

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   root 'messages_controller#index'  #ルートパスの指定
   get 'messages_controller' => 'messages_controller#index'
   resources :users, only: [:edit, :update]
+  resources :hoge
 end
