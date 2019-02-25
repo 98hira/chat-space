cf8ad9710dbd14bb107c8f6248bf07b4b9d68404
98hira <flattail2018@gmail.com>
2019-02-25 14:21:29 +0900

    [add]ユーザー情報編集機能を追加
    [time]2019/02/25-14:22

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root 'messages_controller#index'  #ルートパスの指定
   get 'messages_controller' => 'messages_controller#index'
+  resources :users, only: [:edit, :update]
 end
