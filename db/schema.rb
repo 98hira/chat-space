bb44f0f1424875987cd676462e26207bd99079f4
98hira <flattail2018@gmail.com>
2019-02-25 21:04:13 +0900

    [add]group_userテーブルを追加
    rails g model group_user
    で生成されるマイグレーションファイルにカラムを追加し
    rake db:migrete
    コマンドを実行した結果を登録。
    
    [time]2019/02/25-21:04

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_25_101042) do
+ActiveRecord::Schema.define(version: 2019_02_25_114733) do
+
+  create_table "group_users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "group_id"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["group_id"], name: "index_group_users_on_group_id"
+    t.index ["user_id"], name: "index_group_users_on_user_id"
+  end
 
   create_table "groups", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -33,4 +42,6 @@ ActiveRecord::Schema.define(version: 2019_02_25_101042) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "group_users", "groups"
+  add_foreign_key "group_users", "users"
 end
