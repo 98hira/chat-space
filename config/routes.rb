d425c3a879c44ec44b6bccbd2bd97bf838d80564
98hira <flattail2018@gmail.com>
2019-02-26 15:28:23 +0900

    [add]groupsコントローラへのルーティング設定追加
    [time]2019/02/26-15:28

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   root 'messages_controller#index'  #ルートパスの指定
   get 'messages_controller' => 'messages_controller#index'
   resources :users, only: [:edit, :update]
-  resources :hoge
+  resources :groups, only: [:new, :create, :edit, :update]
 end
