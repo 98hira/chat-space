bb52d2761a11efce795554dcb9bbb3a27cec3dc9
98hira <flattail2018@gmail.com>
2019-02-27 16:42:02 +0900

    [add]グループ情報編集機能を追加。
    以下のように直接アクセスして、グループ編集できることは確認済み。
    http://localhost:3000/groups/[グループID]/edit
    
    [time]2019/02/27-16:42

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,4 +1,6 @@
 class GroupsController < ApplicationController
+  before_action :set_group, only: [:edit, :update]
+
   def index
   end
 
@@ -20,10 +22,21 @@ class GroupsController < ApplicationController
   end
 
   def update
+    if @group.update(group_params)
+      # redirect_to group_messages_path(@group), notice: 'グループを編集しました'
+      redirect_to '/', notice: 'グループを編集しました'
+    else
+      render :edit
+    end
   end
 
   private
+
   def group_params
     params.require(:group).permit(:name, user_ids:[])
+  end
+
+  def set_group
+    @group = Group.find(params[:id])
   end
 end
