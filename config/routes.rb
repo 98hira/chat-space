101f01895e7503dd22266d799143316078aac72c
98hira <flattail2018@gmail.com>
2019-02-27 19:18:41 +0900

    [add]仕様に合わせてルートパスを変更
    ルートパスではサイドバーだけを表示する仕様に合わせて変更
    
    [time]2019/02/27-19:20

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'messages_controller#index'  #ルートパスの指定
+  root 'groups#index'
   get 'messages_controller' => 'messages_controller#index'
   resources :users, only: [:edit, :update]
   resources :groups, only: [:new, :create, :edit, :update]
