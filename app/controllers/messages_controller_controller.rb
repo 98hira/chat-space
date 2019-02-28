3828c9b13f542f0df6a999a892a9f77a52a7cdef
98hira <flattail2018@gmail.com>
2019-02-28 19:01:14 +0900

    [fix]messages_controllerからmessagesにリネーム
    rails g controllerコマンドでコントロラーを作成すると
    末尾に_controllerが追加されるのを失念して、
    次のコマンドを実行してコントローラを作成していため
    リネームする。
    rails g controller messages_controller
    
    [time]2019/02/28-19:03

--- app/controllers/messages_controller_controller.rb
+++
@@ -1,7 +0,0 @@
-class MessagesControllerController < ApplicationController
-  def index
-  end
-
-  def create
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'groups#index'
-  get 'messages_controller' => 'messages_controller#index'
   resources :users, only: [:edit, :update]
   resources :groups, only: [:new, :create, :edit, :update] do
     resources :messages, only: [:index, :create]
