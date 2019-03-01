03514431231672c52f5833dee5c343c350e4db1b
98hira <flattail2018@gmail.com>
2019-03-01 17:55:39 +0900

    [update]投稿される画像サイズが大きすぎるので小さくする
    [time]2019/03/01-17:56

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -2,7 +2,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
   include CarrierWave::MiniMagick
-  process resize_to_fit: [800, 800]
+  process resize_to_fit: [250, 250]
 
   # Choose what kind of storage to use for this uploader:
   storage :file
