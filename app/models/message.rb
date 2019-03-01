9e4ddd27a0ccfec9bf660871b7ca404dddc71c52
98hira <flattail2018@gmail.com>
2019-03-01 14:54:13 +0900

    [add]メッセージ送信機能を追加
    [time]2019/03/01-14:54

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,4 +3,7 @@ class Message < ApplicationRecord
   belongs_to :user
 
   mount_uploader :image, ImageUploader
+
+  #imageとcontentが両方カラの時はモデルを更新しない。
+  validates :content, presence: true, unless: :image?
 end
