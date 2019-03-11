5496d8ff74d0eba8474176b0a98dee816c0a5dd5
98hira <flattail2018@gmail.com>
2019-03-11 13:32:12 +0900

    [add]メッセージ送信機能の非同期通信化対応
    以下の実装まで完了した。
     ユーザーが入力して送信ボタン押下する
     それをトリガーにしてjQueryのイベント発火させる。
     入力データを取り出してjson形式でサーバーへ送る。
     サーバー側ーで受信データをDBへ保存し、再度送り返す。
     jQuery側で受信しログに出力する。
    [実装残件]
    - jQeury側でHTML生成しブラウザへ追加する処理の実装
    
    [time]2019/03/11-13:38

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,6 +1,32 @@
 $(function() {
+  // function buildMessageHTML(message){
+  //    render @messages;
+  // }
   $('.new_message').on('submit', function(e) {
+    // return;
     e.preventDefault();
-    console.log("click detect");
-  })
-})
+    var formData = new FormData(this);
+    console.log(formData);
+    var url = $(this).attr('action');
+    $.ajax({
+      type: 'POST',
+      url: url,
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(data) {
+      // buildMessageHTML(); かり
+      console.log("respons data");
+      console.log(data);
+      // var html = buildHTML(data);
+      // $('.todos').append(html);
+      $('.new_message')[0].reset();
+      $(".main-form__send").prop("disabled", false);
+    })
+    .fail(function() {
+      alert('メッセージ送信に失敗しました');
+    });
+  });
+});
---
+++ app/views/messages/create.json.jbuilder
@@ -0,0 +1,5 @@
+json.content  @message.content
+json.id @message.id
+json.created_at @message.created_at.strftime("%Y年%m月%d日 %H時%M分")
+json.image @message.image.url
+json.user_name @message.user.nickname
