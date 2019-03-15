59e9c616858ccc0338edc02dab21d0c9b0e828a2
98hira <flattail2018@gmail.com>
2019-03-15 20:16:30 +0900

    [add]検索結果の表示処理を追加
    [time]2019/03/15-20:17

--- app/assets/javascripts/user_search.js
+++ app/assets/javascripts/user_search.js
@@ -1,13 +1,34 @@
 $(function() {
   let pre_input = ""
 
+  //userを検索結果に加える
+  function appendSearchUserResult(user) {
+    let html = `<div class="chat-group-user clearfix">
+                  <p class="chat-group-user__name">${user.nickname}</p>
+                  <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</a>
+                </div>`;
+    $('.user-search-result').append(html);
+  }
+
+  //userを検索結果として表示するかを判定する
+  function judgeSearchUserResult(users) {
+    //チャットメンバーに追加された名前一覧取得する
+    let menber_names = $('.chat-group-user__name').map(function(index, elem){return $(elem).text();}).get();
+
+    users.forEach(function(user) {
+      // チャットメンバーに追加されていないユーザーだけを検索結果に加える
+      if ($.inArray(user.nickname, menber_names) < 0) {
+        appendSearchUserResult(user);
+      }
+    });
+  }
+
   $(document).on('turbolinks:load', function(){
     $('#user-search-field').on('keyup', function(event) {
       event.preventDefault();
       var input = $(this).val();
       if ((input.length > 0) && (pre_input != input)) {
         pre_input = input;
-        console.log(input);
         $.ajax({
           url: '/users',
           type: 'GET',
@@ -16,10 +37,8 @@ $(function() {
         })
         .done(function(users) {
           $('.user-search-result').empty();
-          if (users.length >= 0) {
-            users.forEach(function(user) {
-              console.log(user);
-            });
+          if (users.length > 0) {
+            judgeSearchUserResult(users);
           } else {
             console.log("該当ユーザーなし");
           }
